fd04a61812c88c5945a9e54085faea5571e37eb2
h-yes-oo <hyesoo5115@snu.ac.kr>
2021-08-25 22:39:07 +0900

    folder page template

--- src/pages/FolderPage/index.tsx
+++ src/pages/FolderPage/index.tsx
@@ -5,18 +5,192 @@ import { withRouter, RouteComponentProps } from 'react-router-dom';
 import BaseLayout from 'components/BaseLayout';
 
 import OhMyGod from 'assets/images/OhMyGod.png';
+import Check from 'assets/icons/Check.svg';
+import SortToggleDown from 'assets/icons/SortToggleDown.svg';
+import NewFolder from 'assets/icons/NewFolder.svg';
+import Star from 'assets/icons/Star.svg';
+import Clock from 'assets/icons/Clock.svg';
+import TrashCan from 'assets/icons/TrashCan.svg';
 
 interface Props {}
 
-const Oh = styled.img`
+const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 33px;
   width: 1000px;
-  margin: 30px;
+`;
+
+const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.21;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 40px;
+`;
+
+const ButtonName = styled.div`
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.19;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+  margin-left: 8px;
+`;
+
+const Sort = styled.div`
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.19;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+  margin-right: 8px;
+`;
+
+const ButtonImage = styled.img`
+  height: 12px;
+  width: 12px;
+`;
+
+const FolderImage = styled.img`
+  height: 18px;
+  width: 18px;
+`;
+
+const Box = styled.div`
+  width: 1000px;
+  height: 719px;
+  object-fit: contain;
+  box-shadow: 0 3px 16px 0 rgba(0, 0, 0, 0.08);
+  background-color: #fff;
+  overflow: scroll;
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px;
+  margin: 19px 0 41px;
+`;
+
+const Index = styled.div`
+  border-bottom: #e6e6e6 1px solid;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.21;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+  height: 48px;
+`;
+
+const BoxWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SmallBox = styled.div`
+  width: 310px;
+  height: 70px;
+  object-fit: contain;
+  border-radius: 8px;
+  box-shadow: 0 3px 16px 0 rgba(0, 0, 0, 0.08);
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BoxTitle = styled.div`
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.2;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+  margin-left: -20px;
+  display: flex;
+  align-items: center;
+`;
+
+const BoxImage = styled.img`
+  width: 40px;
+  margin-right: 10px;
 `;
 
 const FolderPage: FC<Props> = () => {
   return (
     <BaseLayout grey>
-      <Oh src={OhMyGod} />
+      <Root>
+        <Top>
+          내 학습 노트
+          <ButtonWrapper>
+            <Button>
+              <FolderImage src={NewFolder} />
+              <ButtonName>새폴더</ButtonName>
+            </Button>
+            <Button>
+              <ButtonImage src={Check} />
+              <ButtonName>파일 선택하기</ButtonName>
+            </Button>
+            <Button>
+              <Sort>정렬 기준</Sort>
+              <ButtonImage src={SortToggleDown} />
+            </Button>
+          </ButtonWrapper>
+        </Top>
+        <Box>
+          <Index>노트 제목</Index>
+        </Box>
+        <BoxWrapper>
+          <SmallBox>
+            <BoxTitle>
+              <BoxImage src={Star} />
+              중요 노트함
+            </BoxTitle>
+          </SmallBox>
+          <SmallBox>
+            <BoxTitle>
+              <BoxImage src={Clock} />
+              최근 노트함
+            </BoxTitle>
+          </SmallBox>
+          <SmallBox>
+            <BoxTitle>
+              <BoxImage src={TrashCan} />
+              휴지통
+            </BoxTitle>
+          </SmallBox>
+        </BoxWrapper>
+      </Root>
     </BaseLayout>
   );
 };
