3a7e20a7e1741f9a5b0b604a2c69d8f9cc6bfff1
h-yes-oo <hyesoo5115@snu.ac.kr>
2021-09-18 21:48:14 +0900

    revise deploy.sh and docker settings

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY package.json ./
 
 RUN npm install
 
-COPY ./ ./
+COPY ./src ./
 
 CMD ["npm", "run", "start"]
--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "Hello Docker-compose"
 
 cd /home/ec2-user/deploy/visionnote-mac
 
-docker-compose up -d
+docker-compose up -d --build
 
 echo "Docker-compose started background"
